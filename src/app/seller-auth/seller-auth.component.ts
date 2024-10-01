d9a741d5a7399b18129ca8921d5d221fbae3a412
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-01 23:24:51 +0530

    9 login & signup impl.

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -3,20 +3,32 @@ import { FormsModule } from '@angular/forms';
 import { SellerService } from '../services/seller.service';
 import { Router } from '@angular/router';
 import { SignUp } from '../data-type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css',
 })
 export class SellerAuthComponent {
   constructor(private seller: SellerService, private router: Router) {}
+  showLogin = false;
   ngOnInit(): void {
     this.seller.reloadSeller();
   }
   signUp(data: SignUp): void {
     this.seller.userSignUp(data);
+  }
+  login(data: SignUp): void {
+    console.log(data);
+    // this.seller.userLogin(data);
+  }
+  openLogin() {
+    this.showLogin = true;
+  }
+  openSignUp() {
+    this.showLogin = false;
   }
 }
