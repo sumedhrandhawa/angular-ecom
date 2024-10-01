cf681791bfd5e109a55cd1850615d4d165c212c9
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-01 16:08:30 +0530

    7 SignUp Implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'seller-auth',
         component:SellerAuthComponent
+    },
+    {
+        path:'seller-home',
+        component:SellerHomeComponent
     }
 ];
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './seller-auth.component.html',
-  styleUrl: './seller-auth.component.css'
+  styleUrl: './seller-auth.component.css',
 })
 export class SellerAuthComponent {
-  signUp(data:object):void{
-    console.warn(data);
+  constructor(private seller: SellerService, private router: Router) {}
+  signUp(data: SignUp): void {
+    this.seller.userSignUp(data).subscribe((result) => {
+      if (result) {
+        this.router.navigate(['seller-home']);
+      }
+    });
   }
 }
