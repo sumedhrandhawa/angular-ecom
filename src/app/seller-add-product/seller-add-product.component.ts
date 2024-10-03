540ce4d9b7343fb2c84f29a24091b20c74e9fd54
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-03 15:55:38 +0530

    #13 + ngModel vs [(ngModel)] to fetch value on submit vs on the spot from the form field + Add new product form.

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-add-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-add-product.component.html',
-  styleUrl: './seller-add-product.component.css'
+  styleUrl: './seller-add-product.component.css',
 })
 export class SellerAddProductComponent {
-
+  submit(data: object) {
+    console.warn(data);
+  }
 }
