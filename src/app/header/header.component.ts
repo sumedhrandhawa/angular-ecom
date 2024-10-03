8e24e47a7e6a10c16ad87c7cb214fb9420f15f29
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-03 12:49:54 +0530

    11 *ngIf + #sellerLogin + 'ngForm' + [ngSwitch] + *ngSwitchCase + routerLink + subscribe + route events for url capture +  LoginIn & SignUp flow + tab switching on the basis of conditions.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  menuType: string = 'default';
+  constructor(private route: Router) {}
 
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.warn('in seller area');
+          this.menuType = 'seller';
+        } else {
+          console.warn('outside seller');
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
