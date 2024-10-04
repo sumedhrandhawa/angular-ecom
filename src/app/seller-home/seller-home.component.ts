1837e5cf84d8b4c0f904f787a2f0e872663252d2
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-04 23:37:25 +0530

    #17 + fontAwesome configured with delete icon.

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../data-type';
 import { CommonModule } from '@angular/common';
-
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-seller-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './seller-home.component.html',
   styleUrl: './seller-home.component.css',
 })
 export class SellerHomeComponent {
   productList: undefined | Product[];
   productMessage: undefined | string;
+  icon = faTrash;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
