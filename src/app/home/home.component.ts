067b06d67322fd65c73fa0bd9615f5980361dd9e
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-07 02:08:03 +0530

    #21 + adding and configuring BootStrap and Carousel in angular project.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
