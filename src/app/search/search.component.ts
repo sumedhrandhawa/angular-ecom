472cd7afbfd330dabadff62e7928ffef0f3df1bd
sumedhrandhawa <sumedhrandhawa97@gmail.com>
2024-10-09 00:06:29 +0530

    #27 + ActivatedRoute using this.activeRoute.snapshot.paramMap.get('query') to fetch data from query or /:id for that matter.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
-
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private product: ProductService
+  ) {}
+  searchResult: undefined | Product[];
+  ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    query &&
+      this.product.searchProducts(query).subscribe((result) => {
+        this.searchResult = result;
+      });
+  }
 }
